49f81e25f1fc68c45d0c173310c7849bb9514f22
Kieran Rimmer <kieranrimmer@gmail.com>
2018-04-13 21:12:34 +1000

    JSON Response Bodies: ResponseEntity<String> response bodies at endpoint /jsonSample  rendered with escape character

--- initial/src/main/java/hello/HelloController.java
+++ initial/src/main/java/hello/HelloController.java
@@ -2,6 +2,9 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 @RestController
 public class HelloController {
@@ -9,6 +12,12 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping(value = "/jsonSample", method = RequestMethod.GET, produces = {"application/json"})
+    public ResponseEntity<String> getJson() {
+        String sampleJson = "{\"message\": \"this is json\", \"code\": 200}";
+        return new ResponseEntity<String>(sampleJson, HttpStatus.OK);
     }
     
 }
