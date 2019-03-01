321e777350c3f23812d98411cb9e3612fc02cad5
Kieran Rimmer <kieranrimmer@gmail.com>
2019-03-02 09:10:40 +1100

    Test tuning further

--- complete/src/test/java/obfuscate/service/GCSProviderTest.java
+++ complete/src/test/java/obfuscate/service/GCSProviderTest.java
@@ -1,25 +1,18 @@
 package obfuscate.service;
 
-import com.google.cloud.WriteChannel;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.util.concurrent.Executors;
-
 import org.powermock.api.mockito.PowerMockito;
 
 import static org.mockito.Mockito.*;
-
-import static org.powermock.configuration.ConfigurationType.PowerMock;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({BlobId.class, BlobInfo.class})
@@ -32,15 +25,6 @@ public class GCSProviderTest {
     @Before
     public void setup() {
 
-        // as per https://stackoverflow.com/questions/16170572/unable-to-mock-service-class-in-spring-mvc-controller-tests
-        // this must be called for the @Mock annotations above to be processed
-        // and for the mock service to be injected into the controller under
-        // test.
-
-
-
-        MockitoAnnotations.initMocks(this);
-
         storage = mock(Storage.class);
         serviceUnderTest = new GCSProvider(storage);
 
@@ -48,6 +32,7 @@ public class GCSProviderTest {
 
     @Test
     public void testGetWriter() throws Exception {
+
         PowerMockito.mockStatic(BlobInfo.class);
         PowerMockito.mockStatic(BlobId.class);
 
@@ -55,8 +40,6 @@ public class GCSProviderTest {
         BlobInfo mockBlobInfo = mock(BlobInfo.class);
 
         when(BlobId.of(any(), any())).thenReturn(mockBlobId);
-
-        // BlobInfo blobInfo = BlobInfo.newBuilder(any(BlobId.class)).setContentType("text/plain").build();
 
         BlobInfo.Builder mockBuilder = mock(BlobInfo.Builder.class);
         when(BlobInfo.newBuilder(any(BlobId.class))).thenReturn(mockBuilder);
@@ -67,11 +50,6 @@ public class GCSProviderTest {
         serviceUnderTest.getWriter("bucketName", "blobName");
 
         verify(storage, times(1)).writer(mockBlobInfo);
-
-
-
-
-
 
     }
 
--- complete/src/test/java/obfuscate/service/MacServiceTest.java
+++ complete/src/test/java/obfuscate/service/MacServiceTest.java
@@ -1,4 +1,53 @@
 package obfuscate.service;
 
+import com.google.cloud.storage.Storage;
+import org.apache.commons.codec.digest.HmacUtils;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.Mockito.mock;
+
+import static org.mockito.Mockito.*;
+
 public class MacServiceTest {
+
+    private MacService serviceUnderTest;
+
+    private static String DUMMY_KEY_STRING = "2EF861AC0000702D2EF861AC0000702D2EF861AC0000702D2EF861AC0000702D";
+
+    @Before
+    public void setup() {
+
+        serviceUnderTest = new MacService();
+
+    }
+
+    @Test
+    public void testGetRedactedCSVRecordAsString() throws Exception {
+
+        String dummyPlaintest = "David Boon";
+        int dummySize = 5;
+
+        Set<Integer> colSet = new HashSet<>();
+        colSet.add(1);
+        colSet.add(2);
+
+
+
+        CSVRecord dummyRecord = mock(CSVRecord.class);
+        when(dummyRecord.get(anyInt())).thenReturn(dummyPlaintest);
+        when(dummyRecord.size()).thenReturn(dummySize);
+
+        HmacUtils hmacUtils = serviceUnderTest.getHMACUtils(DUMMY_KEY_STRING.getBytes());
+        String output = serviceUnderTest.getRedactedCSVRecordAsString(hmacUtils, dummyRecord, colSet);
+
+        assert output != null;
+
+    }
+
 }
